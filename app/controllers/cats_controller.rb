0ef390971aafa9d19dbded9e54ce202cbdfcbf9c
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-22 08:01:35 +0200

    Test :redirect_to with status: :see_other (303)

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -53,6 +53,7 @@ class CatsController < ApplicationController
     @cat.destroy
 
     respond_to do |format|
+      # format.html { redirect_to cats_url, notice: "Cat was successfully destroyed.", status: :see_other }
       format.html { redirect_to cats_url, notice: "Cat was successfully destroyed." }
       format.json { head :no_content }
     end
