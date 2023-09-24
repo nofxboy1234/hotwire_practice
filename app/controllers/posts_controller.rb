a32d17a2fe8ec686601b20eaf7cd18fd4e4c435e
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-24 18:57:02 +0200

    Refactor posts_controller and post #index and #new views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,33 +6,31 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
-  def show
-  end
-
   # GET /posts/new
   def new
     @post = Post.new
-  end
-
-  # GET /posts/1/edit
-  def edit
   end
 
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
     end
   end
+
+  # GET /posts/1 or /posts/1.json
+  def show
+  end
+
+
+  # GET /posts/1/edit
+  def edit
+  end
+
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
