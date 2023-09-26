f43d22c3d28f0d815933d7aa458edd606f4e8bfd
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-26 14:25:40 +0200

    Fix innerHtml -> innerHTML spelling error

--- app/javascript/controllers/greeter_controller.js
+++ app/javascript/controllers/greeter_controller.js
@@ -3,7 +3,6 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
   static targets = ['name', 'output'];
   greet() {
-    console.log('greet()')
-    this.outputTarget.innerHtml = `Hello ${this.nameTarget.value}!`;
+    this.outputTarget.innerHTML = `Hello ${this.nameTarget.value}!`;
   }
 }
