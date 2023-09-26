950a297cd85c8f63252a76ed31e2fb01004819a6
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-26 15:40:03 +0200

    Add Stimulus class attribute

--- app/javascript/controllers/greeter_controller.js
+++ app/javascript/controllers/greeter_controller.js
@@ -1,8 +1,9 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['name', 'output'];
+  static targets = ['name', 'output', 'classdiv'];
   static values = { count: { type: Number, default: 0 } };
+  static classes = ['change'];
 
   connect() {
     this.countValue = 5;
@@ -19,5 +20,12 @@ export default class extends Controller {
   greet() {
     this.outputTarget.innerHTML = `Hello ${this.nameTarget.value}!`;
     this.addOne();
+  }
+
+  toggle() {
+    console.log(this.element);
+    console.log(this.classdivTarget)
+    this.classdivTarget.classList.toggle(this.changeClass);
+    this.element.classList.toggle(this.changeClass);
   }
 }
