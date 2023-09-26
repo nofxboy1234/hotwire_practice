57684a7abb6c0eec5726260eb2da8fdda76287f5
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-26 14:48:28 +0200

    Test out Stimulus value attributes

--- app/javascript/controllers/greeter_controller.js
+++ app/javascript/controllers/greeter_controller.js
@@ -2,7 +2,18 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
   static targets = ['name', 'output'];
+  static values = { count: { type: Number, default: 0 } };
+
+  addOne() {
+    this.countValue++;
+  }
+
+  countValueChanged() {
+    console.log(this.countValue);
+  }
+
   greet() {
     this.outputTarget.innerHTML = `Hello ${this.nameTarget.value}!`;
+    this.addOne();
   }
 }
